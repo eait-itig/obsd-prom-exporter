801f8b90c8c689e739c844c8d7e9bc09f64251dd
Li <lilly23@outlook.com>
2025-07-18 14:26:47 +1000

    add timeout stats

--- collect_procs.c
+++ collect_procs.c
@@ -35,8 +35,13 @@
 
 #include "metrics.h"
 #include "log.h"
+#include <sys/timeout.h>
 
 struct procs_modpriv {
+	struct timeoutstat tstats;
+	struct metric *added, *cancelled, *deleted, *late;
+	struct metric *pending, *readded, *rescheduled, *run_softclock;
+	struct metric *run_thread, *scheduled, *softclocks, *thread_wakeups;
 	struct metric *nfiles, *nprocs, *nthreads;
 	struct metric *maxfiles, *maxproc, *maxthread;
 };
@@ -54,6 +59,54 @@ procs_register(struct registry *r, void **modpriv)
 	priv = calloc(1, sizeof (struct procs_modpriv));
 	*modpriv = priv;
 
+	priv->added = metric_new(r, "timeouts_added",
+	    "timeout_add*(9) calls",
+	    METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+	    NULL);
+	priv->cancelled = metric_new(r, "timeouts_cancelled",
+            "dequeued during timeout_del*(9)",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->deleted = metric_new(r, "timeouts_deleted",
+            "timeout_del*(9) calls",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->late = metric_new(r, "timeouts_late",
+            "run after deadline",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->pending = metric_new(r, "timeouts_pending",
+            "number currently ONQUEUE",
+            METRIC_GAUGE, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->readded = metric_new(r, "timeouts_readded",
+            "timeout_add*(9) + already ONQUEUE",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->rescheduled = metric_new(r, "timeouts_rescheduled",
+            "bucketed + already SCHEDULED",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->run_softclock = metric_new(r, "timeouts_run_softclock",
+            "run from softclock()",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->run_thread = metric_new(r, "timeouts_run_thread",
+            "run from softclock_thread()",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->scheduled = metric_new(r, "timeouts_scheduled",
+            "bucketed during softclock()",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->softclocks = metric_new(r, "timeouts_softclocks",
+            "softclock() calls",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
+	priv->thread_wakeups = metric_new(r, "timeouts_thread_wakeups",
+            "wakeups in softclock_thread()",
+            METRIC_COUNTER, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
+            NULL);
 	priv->nfiles = metric_new(r, "system_files_open",
 	    "Total number of files open on the system",
 	    METRIC_GAUGE, METRIC_VAL_UINT64, NULL, &procs_metric_ops,
@@ -135,6 +188,25 @@ procs_collect(void *modpriv)
 		return (0);
 	}
 	metric_update(priv->maxthread, (uint64_t)v);
+
+	size = sizeof(priv->tstats);
+	mib[1] = KERN_TIMEOUT_STATS;
+	if (sysctl(mib, 2, &priv->tstats, &size, NULL, 0) == -1) {
+		tslog("failed to get stats: %s", strerror(errno));
+		return (0);
+	}
+	metric_update(priv->added, priv->tstats.tos_added);
+	metric_update(priv->cancelled, priv->tstats.tos_cancelled);
+	metric_update(priv->deleted, priv->tstats.tos_deleted);
+	metric_update(priv->late, priv->tstats.tos_late);
+	metric_update(priv->pending, priv->tstats.tos_pending);
+	metric_update(priv->readded, priv->tstats.tos_readded);
+	metric_update(priv->rescheduled, priv->tstats.tos_rescheduled);
+	metric_update(priv->run_softclock, priv->tstats.tos_run_softclock);
+	metric_update(priv->run_thread, priv->tstats.tos_run_thread);
+	metric_update(priv->scheduled, priv->tstats.tos_scheduled);
+	metric_update(priv->softclocks, priv->tstats.tos_softclocks);
+	metric_update(priv->thread_wakeups, priv->tstats.tos_thread_wakeups);
 
 	return (0);
 }
